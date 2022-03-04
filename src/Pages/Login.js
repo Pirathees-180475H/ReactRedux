2e386607e678704a6c2b2ed5aeebaa3d13b35932
Pirathees <kamalanathanpirathees@gmail.com>
2022-03-04 10:40:43 +0530

    Log in UI Added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,67 @@
-import React from 'react'
-import { Link, NavLink,useNavigate } from 'react-router-dom'
-import { Button } from 'reactstrap'
+import React, { useState } from 'react'
+import {useNavigate } from 'react-router-dom'
+import { Button ,ButtonGroup,FormGroup,Label,Input} from 'reactstrap'
 
 function Login() {
   let navigate=useNavigate();
 
+  const [userType,setUserType]=useState("Donor");
+  const [userName,setUserName]=useState("");
+  const [password,setPassword]=useState("");
+
+  const changeHandler=(type,event)=>{
+      console.log(type)
+      switch(type){
+        case "userName":{
+          setUserName(event.target.value)
+          break;
+        }
+        case "password":{
+          setPassword(event.target.value)
+          break;
+        }
+      }
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='container'>
+      <div className='jumbotron'>
+        <h2>Welcome to Blood Bank Management System</h2>  
+
+          <div>
+            <h1>{userType}-Login</h1>
+            <FormGroup>
+                    <Label for="userName">
+                        User Name
+                    </Label>
+                    <Input  value={userName} onChange={(e)=>changeHandler("userName",e)} />
+                    
+              </FormGroup>
+
+              <FormGroup>
+                    <Label for="Id">
+                       Password
+                    </Label>
+                    <Input type='password' value={password} onChange={(e)=>changeHandler("password",e)} />
+                    
+              </FormGroup>
+
+          </div>
+         
+
+         <ButtonGroup>
+            <Button onClick={()=>{setUserType("Donor")}}>
+              Donor
+            </Button >
+
+            <Button onClick={()=>{setUserType("Hospital")}}>
+              Hospital
+            </Button>
+            <Button onClick={()=>{setUserType("Admin")}}>
+              Admin
+            </Button>
+          </ButtonGroup>
+      </div>
       <Button color='primary'  size='lg' onClick={()=>navigate("/register")} >Register</Button>
     </div>
   )
